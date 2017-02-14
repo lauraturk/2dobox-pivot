0aecdf6258e2b9cb11287fd45c4a94857e7aceb7
Daniel Bucket <danieljbucket@gmail.com>
2017-02-14 14:09:37 -0700

    four lines js added

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
-const ideaTitle = $('.idea-title')
-const ideaBody = $('.idea-body')
-const saveBtn = $('.save-btn')
-const deleteBtn = $('.delete-btn')
-const upVote = $('.up-vote')
-const downVote = $('.down-vote')
-
+var deleteBtn = $('.delete-btn')
+var upVote = $('.up-vote')
+var downVote = $('.down-vote')
 var cardTitle = $('.card-title')
 var cardBody = $('.card-body')
+
+$('.save-button').click(function() {
+  var ideaTitle = $('.idea-title').val()
+  var ideaBody = $('.idea-body').val()
+  IdeaObj(ideaTitle,ideaBody)
+})
+
+function IdeaObj(id,title,body) {
+  this.id = id
+  this.title = title
+  this.body = body
+}
+
+
+
+console.log(ideaTitle,'ideaTitle')
