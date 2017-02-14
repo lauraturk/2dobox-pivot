288fb3d68540f83d113220086cc4b90e6b049c8b
Daniel Bucket <danieljbucket@gmail.com>
2017-02-13 17:42:47 -0700

    targeting all the necessary bits

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const ideaTitle = $('.idea-title')
+const ideaBody = $('.idea-body')
+const saveBtn = $('.save-btn')
+const deleteBtn = $('.delete-btn')
+const upVote = $('.up-vote')
+const downVote = $('.down-vote')
+
+var cardTitle = $('.card-title')
+var cardBody = $('.card-body')
