f23b8b486737780a8fe0ab72501bf221174ea40d
Daniel Bucket <danieljbucket@gmail.com>
2017-02-15 17:34:19 -0700

    upvote down vote finally working. fuck me

--- scripts.js
+++ scripts.js
@@ -7,18 +7,18 @@ function IdeaObj(id,ideaTitle,ideaBody) {
 
 function newIdea(parsedOut) {
   $('.idea-card-container').prepend(
-    `<div class="idea-card" id="${parsedOut.id}">
-      <div class="card-title-box">
-        <h1 class="card-title">${parsedOut.title}</h1>
-        <button class="delete-btn" type="button" name="button"><img class="quality-image" src="./images/delete.svg" alt="delete button"></img></button>
-      </div>
-      <p class="card-body">${parsedOut.body}</p>
-      <div class="quality-box">
-        <button class="quality-btns up-vote" type="button" name="button"><img class="quality-image" src="./images/upvote.svg" alt="up vote button"></button>
-        <button class="quality-btns down-vote" type="button" name="button"><img class="quality-image" src="./images/downvote.svg" alt="down vote button"></button>
-        <p class="quality-result">Quality: <span class="current-quality">${parsedOut.quality}</span></p>
-      </div>
-    </div>`)
+      `<div class="idea-card" id="${parsedOut.id}">
+        <div class="card-title-box">
+          <h1 class="card-title">${parsedOut.title}</h1>
+          <button class="delete-btn" type="button" name="button"><img class="quality-image" src="./images/delete.svg" alt="delete button"></img></button>
+          </div>
+          <p class="card-body">${parsedOut.body}</p>
+          <div class="quality-box">
+          <button class="quality-btns up-vote" type="button" name="button"><img class="quality-image" src="./images/upvote.svg" alt="up vote button"></button>
+          <button class="quality-btns down-vote" type="button" name="button"><img class="quality-image" src="./images/downvote.svg" alt="down vote button"></button>
+          <p class="quality-result">Quality: <p class="current-quality">${parsedOut.quality}</p></p>
+        </div>
+      </div>`)
   }
 
 // function activate() {
@@ -53,28 +53,32 @@ $('.idea-card-container').on('click', '.delete-btn', function() {
   localStorage.removeItem(sensitive)
 })
 
-persistMafk()
+$('.idea-card-container').on('click', '.down-vote', function() {
+  var changeQuality = $(this).parents('.idea-card').attr('id')
+  var changeThisQuality = JSON.parse(localStorage.getItem(changeQuality))
+  var newQual = $(this).siblings('.current-quality')
+  if (newQual.text() == 'genius') {
+    newQual.text('plausible')
+  } else if (newQual.text() == 'plausible') {
+    newQual.text('swill')
+  }
+  changeThisQuality.quality = newQual.text()
+  localStorage.setItem(changeQuality, JSON.stringify(changeThisQuality))
+  persistMafk()
+})
 
 $('.idea-card-container').on('click', '.up-vote', function() {
-  var changeQuality = JSON.parse(localStorage.getItem($('.idea-card').attr('id')))
-  var changeQualityId = changeQuality.id
-  if (changeQuality.quality == "plausible") {
-    changeQuality.quality = "genius"
-  } else if (changeQuality.quality == "swill") {
-    changeQuality.quality = "plausible"
+  var changeQuality = $(this).parents('.idea-card').attr('id')
+  var changeThisQuality = JSON.parse(localStorage.getItem(changeQuality))
+  var newQual = $(this).siblings('.current-quality')
+  if (newQual.text() == 'swill') {
+    newQual.text('plausible')
+  } else if (newQual.text() == 'plausible') {
+    newQual.text('genius')
   }
-  localStorage.setItem(changeQualityId, JSON.stringify(changeQuality))
+  changeThisQuality.quality = newQual.text()
+  localStorage.setItem(changeQuality, JSON.stringify(changeThisQuality))
   persistMafk()
 })
 
-$('.idea-card-container').on('click', '.down-vote', function() {
-  var changeQuality = JSON.parse(localStorage.getItem($('.idea-card').attr('id')))
-  var changeQualityId = changeQuality.id
-  if (changeQuality.quality == "genius") {
-    changeQuality.quality = "plausible"
-  } else if (changeQuality.quality == "plausible") {
-    changeQuality.quality = "swill"
-  }
-  localStorage.setItem(changeQualityId, JSON.stringify(changeQuality))
-  persistMafk()
-})
+persistMafk()
