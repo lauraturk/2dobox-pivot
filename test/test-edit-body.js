b6ca1b703304650f0622dc10bf5c7f24a7dcb9ff
Laura Turk <laura.lk.turk@gmail.com>
2017-02-20 18:18:25 -0700

    Complete testing suite

---
+++ test/test-edit-body.js
@@ -0,0 +1,37 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+var driver_fx = new webdriver.Builder()
+  .forBrowser('firefox')
+  .build();
+
+
+searchTest(driver_chr);
+searchTest(driver_fx);
+
+
+function searchTest(driver) {
+  driver.get('file:///Users/lauraturk/Turing/2dobox-pivot/index.html');
+  driver.findElement(By.className('idea-title')).sendKeys('Testing Title');
+  driver.findElement(By.className('idea-body')).sendKeys('Testing Body');
+  driver.findElement(By.className('save-button')).click();
+	driver.findElement(By.className('card-body')).click();
+	driver.findElement(By.className('card-body')).sendKeys(' Edit');
+
+  driver.sleep(3000).then(function() {
+    driver.findElement(By.className('card-body')).getText().then(function(title) {
+      if(title === 'Testing Body Edit') {
+        console.log('Test Passed');
+      } else {
+        console.log('Test Failed');
+      }
+    });
+  });
+    driver.quit();
+  }
---
+++ test/test-edit-title.js
@@ -0,0 +1,37 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+var driver_fx = new webdriver.Builder()
+  .forBrowser('firefox')
+  .build();
+
+
+searchTest(driver_chr);
+searchTest(driver_fx);
+
+
+function searchTest(driver) {
+  driver.get('file:///Users/lauraturk/Turing/2dobox-pivot/index.html');
+  driver.findElement(By.className('idea-title')).sendKeys('Testing Title');
+  driver.findElement(By.className('idea-body')).sendKeys('Testing Body');
+  driver.findElement(By.className('save-button')).click();
+	driver.findElement(By.className('card-title')).click();
+	driver.findElement(By.className('card-title')).sendKeys(' Edit');
+
+  driver.sleep(3000).then(function() {
+    driver.findElement(By.className('card-title')).getText().then(function(title) {
+      if(title === 'Testing Title Edit') {
+        console.log('Test Passed');
+      } else {
+        console.log('Test Failed');
+      }
+    });
+  });
+    driver.quit();
+  }
